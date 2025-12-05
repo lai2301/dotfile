fd879f2c0813d57a06aeb9bcd6929a4ba4c81274
Liam Chan <chanlm1129@gmail.com>
2025-12-05 13:06:37 -0800

    new script for restart swaybc

--- fish/.config/fish/conf.d/03-aliases.fish
+++ fish/.config/fish/conf.d/03-aliases.fish
@@ -6,3 +6,4 @@ alias vi="nvim"
 # Access folder
 alias cddot="cd ~/dotfile"
 alias vipath="vi ~/script/env_file/Paths_fifimove.yaml"
+alias gbdate="git branch $(date "+%Y%m%d");git switch $(date "+%Y%m%d")"
